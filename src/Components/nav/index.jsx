60472566c2e780175a1b69c1e5c51abca624818a
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-07-02 19:19:49 -0400

    Init nav componet

---
+++ src/Components/nav/index.jsx
@@ -0,0 +1,26 @@
+import './styles.css';
+import { Col } from 'react-bootstrap';
+import Thumbnail from '../thumbnail';
+import { Link } from "react-router-dom";
+import React from 'react';
+
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+    return (
+        <Col id='navegation-menu' md={2}>
+          Nav
+          <Thumbnail />
+          <br/>
+          <Link to="/">Home</Link>
+          <br/>
+          <Link to="/about">About</Link>
+        </Col>
+    );
+  }
+}
+
+export default Nav;
