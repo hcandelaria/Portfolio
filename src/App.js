2ddbc0e1b13d9412ac111e5d6becc4fd0ffb365c
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-07-02 19:26:00 -0400

    Init navbar and route to the pages.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route }  from "react-router-dom";
+import { Container, Row } from 'react-bootstrap';
+import Home from './Pages/home';
+import About from './Pages/about';
+import Nav from './Components/nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Container fluid>
+        <Row>
+          <BrowserRouter>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+            </Routes>
+          </BrowserRouter>
+        </Row>
+      </Container>
+    </ThemeProvider>
   );
 }
 
