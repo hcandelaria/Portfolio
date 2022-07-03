cf9b6badaef0a3c592344f489fffe97d51204e6e
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-07-02 20:12:56 -0400

    updated style to see edits easier

--- src/Pages/about/index.jsx
+++ src/Pages/about/index.jsx
@@ -14,6 +14,7 @@ class About extends React.Component {
     return (
       <Col 
         md={10}
+        style={{color:'white'}}
         className="d-flex align-items-center justify-content-center"
         id='aboutPage'>
           About
