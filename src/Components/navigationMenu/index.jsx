96fd992fed58dba01035488f01c7b4a6c435a1f9
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-07-09 13:27:06 -0400

    fixed css and activation

--- src/Components/navigationMenu/index.jsx
+++ src/Components/navigationMenu/index.jsx
@@ -7,10 +7,12 @@ import Thumbnail from "../thumbnail";
 class NavigationMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      location : window.location.pathname,
+    };
   }
   componentDidMount() {
-    // console.log('klk')
+    console.log(this.state.location)
   }
 
   render() {
@@ -26,75 +28,60 @@ class NavigationMenu extends React.Component {
             aria-labelledby={`offcanvasNavbarLabel-expand-md`}
             placement="end"
           >
-            <Nav>
-              <ul id="nav-menu">
-                <li>
-                  <Thumbnail />
-                </li>
-                <Nav.Link eventKey="1">
-                  <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                      isActive ? "link active" : "link"
-                    }
-                  >
-                    <li>
-                      Home <ion-icon name="home-outline" />
-                    </li>
-                  </NavLink>
+            <Nav id="nav-menu" as="ul" defaultActiveKey={this.state.location}>
+              <li>
+                <Thumbnail />
+              </li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "link active" : "link"
+                }
+              >
+                <Nav.Link eventKey="/" as="li" className="link">
+                    Home <ion-icon name="home-outline" />
                 </Nav.Link>
-                <Nav.Link eventKey="2">
-                <NavLink
-                  to="/about"
-                  className={({ isActive }) =>
-                    isActive ? "link active" : "link"
-                  }
-                >
-                  <li>
-                    About <ion-icon name="book-outline"></ion-icon>
-                  </li>
-                  
-                </NavLink>
+              </NavLink>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive ? "link active" : "link"
+                }
+              >
+                <Nav.Link eventKey="/about" as="li" className="link">
+                  About <ion-icon name="book-outline"></ion-icon>
                 </Nav.Link>
-                <Nav.Link eventKey="3">
-                <NavLink
-                  to="/projects"
-                  className={({ isActive }) =>
-                    isActive ? "link active" : "link"
-                  }
-                >
-                  <li>
-                    Projects <ion-icon name="briefcase-outline"></ion-icon>
-                  </li>
-                </NavLink>
+              </NavLink>
+              <NavLink
+                to="/projects"
+                className={({ isActive }) =>
+                  isActive ? "link active" : "link"
+                }
+              >
+                <Nav.Link eventKey="/projects" as="li" className="link">
+                  Projects <ion-icon name="briefcase-outline"></ion-icon>
                 </Nav.Link>
-                <li>
-                  <Container id="nav-footer" className="d-grid gap-2">
-                    <Row>
-                      <Col>
-                        <a
-                          href="https://github.com/hcandelaria"
-                          target="_blank"
-                        >
-                          <ion-icon size="large" name="logo-github" />
-                        </a>
-                        <a
-                          href="https://twitter.com/gabriel66_7"
-                          target="_blank"
-                        >
-                          <ion-icon size="large" name="logo-twitter" />
-                        </a>
-                        <a
-                          href="https://www.linkedin.com/in/hector-candelaria-986347136/"
-                          target="_blank"
-                        >
-                          <ion-icon size="large" name="logo-linkedin" />
-                        </a>
-                      </Col>
-                    </Row>
-                  </Container>
-                </li>
-              </ul>
+              </NavLink>
+              <li>
+                <Container id="nav-footer" className="d-grid gap-2">
+                  <Row>
+                    <Col>
+                      <a href="https://github.com/hcandelaria" target="_blank">
+                        <ion-icon size="large" name="logo-github" />
+                      </a>
+                      <a href="https://twitter.com/gabriel66_7" target="_blank">
+                        <ion-icon size="large" name="logo-twitter" />
+                      </a>
+                      <a
+                        href="https://www.linkedin.com/in/hector-candelaria-986347136/"
+                        target="_blank"
+                      >
+                        <ion-icon size="large" name="logo-linkedin" />
+                      </a>
+                    </Col>
+                  </Row>
+                </Container>
+              </li>
             </Nav>
           </Navbar.Offcanvas>
         </Navbar>
