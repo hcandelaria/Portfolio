13db9eb5b63ff9fbcd4088b55a112cb7bcace0bf
hcandelaria <hcandelaria66.7@gmail.com>
2022-07-15 23:50:03 -0400

    Updated thumbnail image link to use AWS S3.

--- src/Components/thumbnail/index.jsx
+++ src/Components/thumbnail/index.jsx
@@ -1,7 +1,6 @@
 import './styles.css';
 import React from 'react';
 import { Image } from "react-bootstrap";
-import profile from './profile.jpg'; 
 
 class Thumbnail extends React.Component {
   constructor(props) {
@@ -15,7 +14,7 @@ class Thumbnail extends React.Component {
         thumbnail
         roundedCircle
         className='mx-auto d-block'
-        src={profile}
+        src="https://hcandelariapersonalportfolio.s3.amazonaws.com/images/portofolio/profile.jpg"
       />
     );
   }
