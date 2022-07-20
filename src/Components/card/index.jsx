11a692e227dde9b1b0c920597f208f386978fef7
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-07-19 22:56:45 -0400

    Disable unavailable links

--- src/Components/card/index.jsx
+++ src/Components/card/index.jsx
@@ -1,13 +1,10 @@
-import './styles.css';
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import IconLink from '../iconLink';
-import Pill from '../pill';
-
-
+import "./styles.css";
+import React from "react";
+import { Card } from "react-bootstrap";
+import IconLink from "../iconLink";
+import Pill from "../pill";
 
 const Badge = (props) => {
-
   return (
     <Card>
       <Card.Img variant="top" src={props.data.img} />
@@ -15,17 +12,21 @@ const Badge = (props) => {
         <Card.Title>{props.data.name}</Card.Title>
         <Card.Text>{props.data.description}</Card.Text>
         <div>
-        {
-          props.data.stack.map((tech,index) => {
-            return <Pill key={index} tech={tech}/>
-          })
-        }
+          {props.data.stack.map((tech, index) => {
+            return <Pill key={index} tech={tech} />;
+          })}
         </div>
-        <IconLink icon="logo-github" link={props.data.links[1]}/>
-        <IconLink icon="globe-outline" link={props.data.links[0]}/>
+        { props.data.links[0] ?
+          <IconLink icon="logo-github" link={props.data.links[0]} /> :
+          <IconLink icon="logo-github" className="disabled"/>
+        }
+        { props.data.links[1] ?
+          <IconLink icon="globe-outline" link={props.data.links[1]} /> :
+          <IconLink icon="globe-outline" className="disabled"/>
+        }
       </Card.Body>
     </Card>
   );
-}
+};
 
 export default Badge;
--- src/Components/iconLink/index.jsx
+++ src/Components/iconLink/index.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./styles.css";
 
 const IconLink = (props) => {
+
+  let style = props.className ? "icon-link " + props.className : "icon-link" 
   return (
-    <a className="icon-link" href={props.link} target="_blank">
+    <a className={style} href={props.link} target="_blank">
       <ion-icon size="large" name={props.icon} />
     </a>
   );
